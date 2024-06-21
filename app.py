0fae2b440c7ac52840c754358eca9a0e377af722
vedantp2905 <vedantpungliya29@gmail.com>
2024-06-21 19:13:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -172,10 +172,11 @@ def main():
             with st.form('Choose Groq Model'):
                 model_type = st.radio("Choose Your Groq Model if Groq selected",('gemma-7b-it','llama3-70b-8192','llama3-8b-8192','mixtral-8x7b-32768'))
                 submitted = st.form_submit_button()
-
-       api_key = st.text_input(f'Enter your API key', type="password")
-       replicate_api_token = st.text_input('Enter Replicate API key', type="password")
-       submitted = st.form_submit_button()
+         
+       with st.form('Gemini/OpenAI/Groq'):
+           api_key = st.text_input(f'Enter your API key', type="password")
+           replicate_api_token = st.text_input('Enter Replicate API key', type="password")
+           submitted = st.form_submit_button()
 
    # Check if API key is provided and set up the language model accordingly
    if api_key:
